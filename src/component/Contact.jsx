3a3c1fcc4da29651cabe78d5e9117bca890bc087
Yogesh Tundiya <yogeshtundiya945@gmail.com>
2025-02-14 15:15:11 +0530

    small-commit-Y

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,20 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "./shared/Layout.js/Layout";
 import Spinner from "./shared/Spinner";
 import { useSelector } from "react-redux";
+import { Button, Modal } from "react-bootstrap";
+// import Model from "react-bootstrap/Modal";
+// import Model from "./shared/modal/Model";
 
 function Contact() {
   const { error, loading } = useSelector((state) => state.auth);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <Layout>
       {error && <span>{alert(error)}</span>}
       {loading ? (
         <Spinner />
       ) : (
-        <h5 className="ms-4">
-          <i className="fa-solid fa-plus py-4"></i>
-          Add Inventory
-        </h5>
+        <>
+          <h5
+            className="ms-4"
+            variant="primary"
+            onClick={handleShow}
+            style={{ cursor: "pointer" }}
+          >
+            <i className="fa-solid fa-plus py-4"></i>
+            Add Inventory
+          </h5>
+          <Modal
+            show={show}
+            setShow={setShow}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Modal title</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div className="d-flex">Blood type &nbsp;</div>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary">Understood</Button>
+            </Modal.Footer>
+          </Modal>
+        </>
       )}
     </Layout>
   );
---
+++ src/component/shared/modal/Model.js
@@ -0,0 +1,32 @@
+// import React, { useState } from "react";
+// import { Modal, Button } from "react-bootstrap";
+// const Model = () => {
+//   const [show, setShow] = useState(false);
+//   const handleClose = () => setShow(false);
+//   return (
+//     <>
+//       <Modal
+//         show={show}
+//         onHide={handleClose}
+//         backdrop="static"
+//         keyboard={false}
+//       >
+//         <Modal.Header closeButton>
+//           <Modal.Title>Modal title</Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body>
+//           I will not close if you click outside me. Do not even try to press
+//           escape key.
+//         </Modal.Body>
+//         <Modal.Footer>
+//           <Button variant="secondary" onClick={handleClose}>
+//             Close
+//           </Button>
+//           <Button variant="primary">Understood</Button>
+//         </Modal.Footer>
+//       </Modal>
+//     </>
+//   );
+// };
+
+// export default Model;
